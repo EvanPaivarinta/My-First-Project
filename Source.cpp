d5fc71e3ab4ae08040f2a4069a9396e7fdd00a30
EvanPaivarinta <89535702+EvanPaivarinta@users.noreply.github.com>
2021-09-01 13:16:04 -0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+/*
+	Evan Paivarinta
+	C++ Fall 2021
+	Due: September 1st
+	Lab 1 Hello World
+	Creating text on the screen
+	*/
+
+#include <iostream>
+int main()
+{
+	std::cout << "Hello World!";
+	return 0;
+}
